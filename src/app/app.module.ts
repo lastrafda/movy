b0cad814fe656d19a73aea136a4cade86f951297
José Lastra <lastrafda@gmail.com>
2020-06-29 21:24:07 -0400

    Changed folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
+import { HeaderComponent } from './shared';
 
 @NgModule({
   declarations: [
