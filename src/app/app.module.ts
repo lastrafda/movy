5f5e7a6a960643893c414a3a7d0bdb807fb3b019
José Lastra <lastrafda@gmail.com>
2020-07-04 03:55:24 -0400

    Installed font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared';
 import {HttpClientModule} from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     declarations: [
@@ -15,6 +16,7 @@ import {HttpClientModule} from '@angular/common/http';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        FontAwesomeModule,
     ],
     providers: [],
     exports: [],
